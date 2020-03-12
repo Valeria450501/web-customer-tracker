33070afd3e0c856685005b76870686509e0437e1
Popenya_V <Popenya_V@9.145.38.25>
2020-03-12 17:13:06 +0300

    Add new customer

--- src/com/luv2code/springdemo/dao/CutomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CutomerDAOImpl.java
@@ -23,12 +23,19 @@ public class CutomerDAOImpl implements CustomerDAO {
 		Session currentSession = sessionFactory.getCurrentSession();
 		
 		//create a query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName",
+				Customer.class);
 		
 		// execute query & get result list
 		List<Customer> customers = theQuery.getResultList();
 		
 		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		currentSession.save(theCustomer);
 	}
 
 }
